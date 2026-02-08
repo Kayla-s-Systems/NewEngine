97bf04d52ec1f8260501f31e3a4117abb9800a02
CalistaVerner <miomoor182@gmail.com>
2026-02-08 20:19:19 +0300

    AssetManager changed it's repository :sunglasses: + cleanUp

--- neocore2/crates/newengine-AssetManager/src/importers.rs
+++
@@ -1,7 +0,0 @@
-use crate::types::{Asset, AssetError, AssetKey};
-
-pub trait Importer<T: Asset>: Send + Sync + 'static {
-    fn supported_extensions(&self) -> &'static [&'static str];
-
-    fn import(&self, bytes: &[u8], key: &AssetKey) -> Result<T, AssetError>;
-}
