4bbb6a14ee450f854288736ad8ce64dc0b76ba08
CalistaVerner <miomoor182@gmail.com>
2026-02-16 11:52:34 +0300

    Full Scratch

--- neocore2/apps/editor-/src/plugin_manager/mod.rs
+++
@@ -1,7 +0,0 @@
-#![forbid(unsafe_op_in_unsafe_fn)]
-
-pub mod bridge;
-pub mod ui;
-
-pub use bridge::PluginManagerBridge;
-pub use ui::PluginManagerUi;
--- neocore2/apps/editor-/src/render_controller/mod.rs
+++
@@ -1,8 +0,0 @@
-#![forbid(unsafe_op_in_unsafe_fn)]
-
-mod controller;
-mod gpu;
-mod module_impl;
-mod viewport;
-
-pub use controller::EditorRenderController;
